54e85f7eed23af3cc085413023d55a155441fcea
Pena <penawalin@gmail.com>
2024-02-06 16:19:48 +0000

    Switch statement to retrieve and display license badge in generated README file

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,7 +1,35 @@
+function genLicenseBadge(license) {
+    let licenseBadge;
+    switch (license) {
+        case 'MIT':
+            licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case 'Apache 2.0':
+            licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        case 'GPL 3.0':
+            licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            break;
+        case 'BSD 3-Clause':
+            licenseBadge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+            break;
+        case 'None':
+            licenseBadge = '';
+            break;
+        default:
+            licenseBadge = '';
+            break;
+    }
+    return licenseBadge;
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-    return `
+    // Handle license badges
+    const licenseBadge = genLicenseBadge(data.license);
+
+    return `${licenseBadge}
 
 # ${data.title}
 
